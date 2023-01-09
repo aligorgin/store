a11daadb31c0004bec3fceff371db22bf4b94fb9
ali <aliigorgin@gmail.com>
2023-01-10 00:40:02 +0330

    fonts fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,41 +6,40 @@ import '../styles/globals.css';
 const myFont = localFont({
 	src: [
 		{
-			path: './fonts/IRANYekan/iranyekan-thin.ttf',
+			path: './fonts/iranyekan-thin.ttf',
 			weight: '100'
 		},
 		{
-			path: './fonts/IRANYekan/iranyekan-light.ttf',
+			path: './fonts/iranyekan-light.ttf',
 			weight: '200'
 		},
 		{
-			path: './fonts/IRANYekan/iranyekan.ttf',
+			path: './fonts/iranyekan.ttf',
 			weight: '300'
 		},
 		{
-			path: './fonts/IRANYekan/iranyekan-medium.ttf',
+			path: './fonts/iranyekan-medium.ttf',
 			weight: '400'
 		},
 		{
-			path: './fonts/IRANYekan/iranyekan-bold.ttf',
+			path: './fonts/iranyekan-bold.ttf',
 			weight: '500'
 		},
 		{
-			path: './fonts/IRANYekan/iranyekan-black.ttf',
+			path: './fonts/iranyekan-black.ttf',
 			weight: '600'
 		},
 		{
-			path: './fonts/IRANYekan/iranyekan-extrabold.ttf',
+			path: './fonts/iranyekan-extrabold.ttf',
 			weight: '700'
 		},
 		{
-			path: './fonts/IRANYekan/iranyekan-extrablack.ttf',
+			path: './fonts/iranyekan-extrablack.ttf',
 			weight: '800'
 		}
 	],
 	variable: '--my-font'
 });
-
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
