d1b76e324f0940031b669c39fc719b4272e13a17
ali <aliigorgin@gmail.com>
2023-01-23 23:23:02 +0330

    tic tac toe finised

--- src/pages/pt.tsx
+++ src/pages/pt.tsx
@@ -52,18 +52,19 @@ function Board({ xIsNext, squares, onPlay }: { xIsNext: boolean; squares: any; o
 }
 
 export default function Game() {
-	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
-	const currentSquares = history[history.length - 1];
-	
+	const [currentMove, setCurrentMove] = useState(0);
+	const xIsNext = currentMove % 2 === 0;
+	const currentSquares = history[currentMove];
+
 	function handlePlay(nextSquares: any) {
-		setHistory([...history, nextSquares]);
-		setXIsNext(!xIsNext);
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		setHistory(nextHistory);
+		setCurrentMove(nextHistory.length - 1);
 	}
 
 	function jumpTo(nextMove: any) {
-		// console.log(nextMove);
-		// console.log(history);
+		setCurrentMove(nextMove);
 	}
 
 	const moves = history.map((squares, move) => {
